eecd897ed490393c440c432f643209c0d9969213
misacekk <pobuda.michal6@seznam.cz>
2025-03-03 20:31:40 +0100

    Update Fridge.java

--- src/elektroshop/Fridge.java
+++ src/elektroshop/Fridge.java
@@ -6,17 +6,45 @@ public class Fridge {
     private int rokVyroby;
     private Narocnost narocnost;
 
-public Fridge(String znacka, String model, int rokVyroby,Narocnost narocnost){
-    this.znacka = znacka;
-    this.model = model;
-    this.rokVyroby = rokVyroby;
-    this.narocnost = narocnost;
-}
-public int getRokVyroby() {
-    return rokVyroby;
-}
-public void printInfo(){
-        System.out.print("Rok výroby: "+getRokVyroby());
+    public Fridge(String znacka, String model, int rokVyroby, Narocnost narocnost) {
+        this.znacka = znacka;
+        this.model = model;
+        this.rokVyroby = rokVyroby;
+        this.narocnost = narocnost;
+    }
 
-}
+    public int getRokVyroby() {
+        return rokVyroby;
+    }
+
+    public Narocnost getNarocnost() {
+        return narocnost;
+    }
+
+    public void printInfo() {
+        System.out.print(getRokVyroby() + ": ");
+        switch (narocnost) {
+            case A:
+                System.out.println("very low");
+                break;
+            case B:
+                System.out.println("low");
+                break;
+            case C:
+                System.out.println("normal");
+                break;
+            case D:
+                System.out.println("above normal");
+                break;
+            case E:
+                System.out.println("high");
+                break;
+            case F:
+                System.out.println("very high");
+                break;
+            case G:
+                System.out.println("extremely high");
+                break;
+        }
+    }
 }
